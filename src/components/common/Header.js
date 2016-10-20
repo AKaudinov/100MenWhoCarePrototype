ea8ecdd34acb328ba653345b149c842814fd2f2e
Amir Kaudinov <kaudinovamir@gmail.com>
2016-10-20 17:26:16 -0600

    updated to bootstrap 4 alpha 5. Edited the hover styles on the header, and the focus style of the mobile button when the header is collapsed. Restyled the contact us page.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -9,37 +9,37 @@ const Header = () => {
                 <a className="navbar-brand" href="http://www.100whocarealliance.org/">
                     <span><img className="logo" src={require('../../assets/100WhoCareColorado.png')}/></span>
                 </a>
-                <button className="navbar-toggler hidden-md-up pull-xs-right" type="button" data-toggle="collapse"
+                <button className="navbar-toggler hidden-md-up pull-right" type="button" data-toggle="collapse"
                         data-target="#navContent"
                         aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="fa fa-bars"/>
                 </button>
 
-                <div className="collapse navbar-toggleable-sm pull-md-right" id="navContent">
-                    <ul className="nav navbar-nav">
+                <div className="collapse navbar-toggleable-sm" id="navContent">
+                    <ul className="headerMenu nav navbar-nav">
                         <li className="nav-item">
-                            <div/>
+                            <div className="menu-home"/>
                             <IndexLink to="/" activeClassName="active" className="nav-link">
                                 <i className="fa fa-home"/> Home</IndexLink>
                         </li>
                         <li className="nav-item">
-                            <div/>
+                            <div className="menu-about"/>
                             <Link to="/about" activeClassName="active" className="nav-link">
                                 <i className="fa fa-info-circle"/> About</Link>
                         </li>
                         <li className="nav-item">
-                            <div/>
+                            <div className="menu-gallery"/>
                             <a className="nav-link" href="#">
                                 <i className="fa fa-picture-o"/> Gallery
                             </a>
                         </li>
                         <li className="nav-item">
-                            <div/>
+                            <div className="menu-contact"/>
                             <Link to="/contact" activeClassName="active" className="nav-link">
                                 <i className="fa fa-envelope"/> Contact</Link>
                         </li>
                         <li className="nav-item">
-                            <div/>
+                            <div className="menu-events"/>
                             <a href="#" className="nav-link">
                                 <i className="fa fa-calendar"/> Events</a>
                         </li>
