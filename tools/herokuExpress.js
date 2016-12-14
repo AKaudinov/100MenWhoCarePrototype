aaeafb064a5c971652b4d19b9222b42bdea50672
Amir Kaudinov <kaudinovamir@gmail.com>
2016-12-13 17:10:04 -0700

    Adding deployment build process for heroku.

---
+++ tools/herokuExpress.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import compression from 'compression';
+import colors from 'colors';
+
+/*eslint-disable no-unused-vars*/
+/*eslint-disable no-console*/
+
+const port = 8000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(port, err => {
+   if(err) {
+       console.log(err);
+   }else{
+       console.log('listening on port 8000'.green);
+   }
+});
