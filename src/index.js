1a88c6f56ea9a470503ed1c34497ab3bd7c65461
Amir Kaudinov <kaudinovamir@gmail.com>
2017-01-28 10:00:30 -0700

    trying to fix tether io bug with heroku

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './styles/index.scss';
 import '../node_modules/jquery/dist/jquery.min.js';
-import '../node_modules/tether/dist/js/tether.min.js';
+//import '../node_modules/tether/dist/js/tether.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '../node_modules/toastr/build/toastr.min.css';
 
