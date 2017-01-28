f25b13946a9d2dc09eb2eb6b6ea9de0f0a5dc679
Amir Kaudinov <kaudinovamir@gmail.com>
2017-01-27 17:21:33 -0700

    adding redux dev tools wiring to the creation of the dev store.

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,5 +1,5 @@
 //store is configured as the app starts
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
@@ -8,6 +8,8 @@ export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        compose(applyMiddleware(thunk, reduxImmutableStateInvariant()),
+        window.devToolsExtension ? window.devToolsExtension() : f => f)
+        //supplying middleware via commas via parameters as much middleware as needed.
     );
 }
