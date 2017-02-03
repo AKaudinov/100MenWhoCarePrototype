28c61c08b70c8d2c7d214db81ee5ad9f7a710320
Amir Kaudinov <kaudinovamir@gmail.com>
2017-02-02 17:57:43 -0700

    Refactoring the api calls, providing browsers list for the auto prefixer, testing the refactored api calls.

--- src/api/api.js
+++ src/api/api.js
@@ -3,32 +3,56 @@ import fetch from 'isomorphic-fetch';
 const apiUrl = 'https://denver-100menwhocare.herokuapp.com/api/contacts';
 //const localApiUrl = 'http://localhost:3000/api/contacts';
 
-class api {
-    static SendContact(contactInfo){
-        let fetchRequest = new Request(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify(contactInfo),
+const request = (method, body) => {
+    return method.toLowerCase() === 'post' ? new Request(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        mode: 'cors',
+        redirect: 'follow',
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }) :
+        new Request(apiUrl, {
+            method: 'GET',
             mode: 'cors',
             redirect: 'follow',
             headers: new Headers({
                 'Content-Type': 'application/json'
             })
         });
+};
+
+
+class api {
+    static SendContact(contactInfo){
+        //let fetchRequest = new Request(apiUrl, {
+        //    method: 'POST',
+        //    body: JSON.stringify(contactInfo),
+        //    mode: 'cors',
+        //    redirect: 'follow',
+        //    headers: new Headers({
+        //        'Content-Type': 'application/json'
+        //    })
+        //});
+        let fetchRequest = request('post', contactInfo);
         return fetch(fetchRequest)
         .then(response => response.json());
     }
 
     static GetAllContacts(){
-        let fetchRequest = new Request(apiUrl, {
-           method: 'GET',
-            mode: 'cors',
-            redirect: 'follow',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        });
+        //let fetchRequest = new Request(apiUrl, {
+        //   method: 'GET',
+        //    mode: 'cors',
+        //    redirect: 'follow',
+        //    headers: new Headers({
+        //        'Content-Type': 'application/json'
+        //    })
+        //});
+        let fetchRequest = request('get');
         return fetch(fetchRequest)
         .then(response => response.json());
     }
 }
+
 export default api;
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -63,7 +63,6 @@ export default {
             "tether":'tether',
             "Tether":'tether'
         })
-
     ],
     performance:{
         hints: false
