53ca2c38f4198ffc234bccd1891eb3d3998c9176
Amir Kaudinov <kaudinovamir@gmail.com>
2017-02-16 18:38:30 -0700

    refactoring image displayer

--- src/components/Gallery/ImageDispayer.js
+++ src/components/Gallery/ImageDispayer.js
@@ -1,8 +1,8 @@
 import React, {PropTypes} from 'react';
-import base64js from 'base64-js';
+import {fromByteArray} from 'base64-js';
 
 const ImageDisplayer = ({imgId, img}) => {
-    let baseString = base64js.fromByteArray(img);
+    let baseString = fromByteArray(img);
     let imageSource = `data:image/jpeg;charset=utf-8;base64,${baseString}`;
  return(
      <div id={`image${imgId}`} className="gallery-image">
