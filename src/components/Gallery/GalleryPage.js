76570aa372817d9328b66d687906529e261d8427
Amir Kaudinov <kaudinovamir@gmail.com>
2017-02-20 20:30:58 -0700

    Working with new database 'ContentStr' text column in order to display base64 images.

--- src/components/Gallery/GalleryPage.js
+++ src/components/Gallery/GalleryPage.js
@@ -1,15 +1,13 @@
 import React, {PropTypes} from 'react';
 import ImageDisplayer from './ImageDispayer';
 import {fromByteArray} from 'base64-js';
-import base64 from 'base-64';
 
 const GalleryPage = ({gallery}) => {
   let images = (
       gallery.map(image => {
           //let base64Image = fromByteArray(image.Content.data);
-          let base64Image = base64.encode(image.Content.data);
         return (<div key={image.Id} className="col-xs-12 col-sm-6 col-md-3">
-            <ImageDisplayer imgId={image.Id} img={base64Image}/>
+            <ImageDisplayer imgId={image.Id} img={image.ContentStr}/>
         </div>);}));
     return(
       <div className="text-xs-center row">
--- src/components/Gallery/ImageDispayer.js
+++ src/components/Gallery/ImageDispayer.js
@@ -1,8 +1,6 @@
 import React, {PropTypes} from 'react';
-//import {fromByteArray} from 'base64-js';
 
 const ImageDisplayer = ({imgId, img}) => {
-    //let baseString = fromByteArray(img);
     let imageSource = `data:image/jpeg;base64,${img}`;
  return(
      <div id={`image${imgId}`} className="gallery-image">
