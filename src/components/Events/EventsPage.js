ff52691e36ecd77e81b6ff878882da8646eaaa26
Amir Kaudinov <kaudinovamir@gmail.com>
2017-03-09 17:35:28 -0700

    Re-structured the events page from ground up including its styles.

--- src/components/Events/EventsPage.js
+++ src/components/Events/EventsPage.js
@@ -1,34 +1,57 @@
 import React from 'react';
-import '../../../node_modules/react-big-calendar/lib/css/react-big-calendar.css';
-import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import '../../styles/events/events.scss';
-
-BigCalendar.momentLocalizer(moment);
 
 const EventsPage = () => {
     let events = [
         {
             title: 'Daylight Savings',
-            allDay: true,
-            start: moment("2017-03-12"),
-            end: moment("2017-03-13")
+            description: "Don't forget to convert your clocks 1 hour ahead!",
+            start: moment("2017-03-12").format("MM/DD/YYYY"),
+            end: moment("2017-03-13").format("MM/DD/YYYY")
         },
         {
-            title: 'holidays',
-            allDay: true,
-            start: moment("2017-03-21"),
-            end: moment("2017-03-23")
+            title: 'Holidays',
+            description: 'Get some holidays in!',
+            start: moment("2017-03-21").format("MM/DD/YYYY"),
+            end: moment("2017-03-23").format("MM/DD/YYYY")
         }
     ];
+
+    let displayEvents =
+        events.map((event, index) => {
+                return (
+                index % 2 == 0 ?
+                    <div key={`item${index}`} className="col-xs-12 event-item">
+                        <div className="event-title text-xs-center text-sm-left col-xs-12 col-sm-4">
+                            <h5>{event.title}</h5>
+                            <div className="event-date">{event.start} - {event.end}</div>
+                        </div>
+                        <div className="event-description text-xs-center text-sm-left col-xs-12 col-sm-8">
+                            <p>{event.description}</p>
+                        </div>
+                    </div>
+                    :
+                    <div key={`item${index}`} className="col-xs-12 event-item">
+                        <div className="event-title-alternate text-xs-center text-sm-left col-xs-12 col-sm-4">
+                            <h5>{event.title}</h5>
+                            <div className="event-date">{event.start} - {event.end}</div>
+                        </div>
+                        <div className="event-description-alternate text-xs-center text-sm-left col-xs-12 col-sm-8">
+                            <p>{event.description}</p>
+                        </div>
+                    </div>
+                );
+            }
+        );
+
     return (
         <div className="jumbotron event-main">
             <div className="events-container container">
                 <h2 className="display-4">Events this month</h2>
                 <hr className="events-horizontal-break-line"/>
-                <div className="events-calendar-view">
-                    <BigCalendar
-                        events={events}/>
+                <div className="events-view row">
+                    {displayEvents}
                 </div>
             </div>
         </div>
