e5a4568afc9cab08fd8d66913277fcd4b809cfdc
Amir Kaudinov <kaudinovamir@gmail.com>
2017-05-11 17:25:59 -0600

    working on the carousel item.

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -51,6 +51,7 @@ class HomePage extends React.Component {
                     </div>
                 </div>
 
+
                 <div className="home-info-jumbo">
                     <div className="jumbotron">
                         <div className="container">
@@ -161,3 +162,47 @@ class HomePage extends React.Component {
 
 
 export default HomePage;
+
+//
+//<div id="carousel-home" className="carousel slide" data-ride="carousel">
+//    <ol className="carousel-indicators">
+//        <li data-target="#carousel-home" data-slide-to="0" className="active"/>
+//        <li data-target="#carousel-home" data-slide-to="1"/>
+//        <li data-target="#carousel-home" data-slide-to="2"/>
+//    </ol>
+//    <div className="carousel-inner" role="listbox">
+//
+//        <div className="carousel-item active">
+//            <img src={require('../../assets/colorado.png')} alt="Colorado"/>
+//            <div className="carousel-caption">
+//                <h4>Test</h4>
+//                <p>First Slide</p>
+//            </div>
+//        </div>
+//
+//        <div className="carousel-item">
+//            <img src={require('../../assets/coloradoMountains.png')} alt="Colorado 2"/>
+//            <div className="carousel-caption">
+//                <h4>Second Slide</h4>
+//                <p>The second slide</p>
+//            </div>
+//        </div>
+//
+//        <div className="carousel-item">
+//            <img src={require('../../assets/denver.png')} alt="Colorado 3"/>
+//            <div className="carousel-caption">
+//                <h4>And one more for good measure</h4>
+//                <p>Third slide for good measure</p>
+//            </div>
+//        </div>
+//
+//        <a className="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+//            <span className="icon-prev" aria-hidden="true"/>
+//            <span className="sr-only">Previous</span>
+//        </a>
+//        <a className="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+//            <span className="icon-next" aria-hidden="true"/>
+//            <span className="sr-only">Next</span>
+//        </a>
+//    </div>
+//</div>
